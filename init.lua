776763d960df2672bd0c73e5e140757c7c1aac51
Megaf <mmegaf@gmail.com>
2014-11-25 21:47:20 +0000

    Files added.

---
+++ init.lua
@@ -0,0 +1,77 @@
+minetest.register_node("mese_fence:mese_fence", {
+	description="Mese Fence - A fence that conducts electrecity.",
+	drawtype = "fencelike",
+	tiles = {"mese_fence.png"},
+	inventory_image = "mese_fence_inv.png",
+	wield_image = "mese_fence_inv.png",
+	paramtype = "light",
+	selection_box = {
+	        type = "fixed",
+	        fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
+	},
+	groups = {snappy=3},
+	drop = 'mese_fence:mese_fence',
+	walkable = true,
+	mesecons = {conductor = {
+		state = mesecon.state.off,
+		rules = {
+			{x = -1, y = 0, z = 0},
+			{x = 1, y = 0, z = 0},
+			{x = 0, y = -1, z = 0},
+			{x = 0, y = 1, z = 0},
+			{x = 0, y = 0, z = -1},
+			{x = 0, y = 0, z = 1},
+		},
+		onstate = "mese_fence:mesefence_active",
+	}}
+})
+
+minetest.register_node("mese_fence:mese_fence_active", {
+	drawtype = "fencelike",
+	tiles = {"mese_fence.png^mese_fence_active.png"},
+	paramtype = "light",
+	selection_box = {
+	        type = "fixed",
+	        fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
+	},
+	groups = {snappy=3},
+	drop = 'mese_fence:mese_fence',
+	walkable = true,
+	mesecons = {conductor = {
+		state = mesecon.state.on,
+		rules = {
+			{x = -1, y = 0, z = 0},
+			{x = 1, y = 0, z = 0},
+			{x = 0, y = -1, z = 0},
+			{x = 0, y = 1, z = 0},
+			{x = 0, y = 0, z = -1},
+			{x = 0, y = 0, z = 1},
+		},
+		offstate = "mese_fence:mese_fence",
+	}}
+})
+
+minetest.register_craft({
+	output = 'mese_fence:mese_fence 4',
+	recipe = {
+		{"group:stick", "group:stick", "group:stick"},
+		{"group:stick", "group:mesecon_conductor_craftable", "group:stick"},
+		{"group:stick", "default:steel_ingot", "group:stick"},
+	}
+})
+
+-- An alias to make things easier.
+minetest.register_alias('mese_fence', 'mese_fence:mese_fence')
+
+-- Backwards compatibility.
+minetest.register_alias('mesecons_mesefence:mese_fence', 'mese_fence:mese_fence')
+minetest.register_alias('mesecons_random:conductingfence', 'mese_fence:mese_fence')
+minetest.register_alias('mesecons_mese_fence:mese_fence', 'mese_fence:mese_fence')
+
+minetest.register_alias('mesecons_mesefence:mese_fence_active', 'mese_fence:mese_fence_active')
+minetest.register_alias('mesecons_random:conductingfence_active', 'mese_fence:mese_fence_active')
+minetest.register_alias('mesecons_mese_fence:mese_fence_active', 'mese_fence:mese_fence_active')
+
+-- Mod loaded message.
+print('[mesecons_mesefence] loaded.')
+
